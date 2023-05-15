4a5b4de5c81684fee0681f68c749c413bb2b16da
bbeizer <89622436+bbeizer@users.noreply.github.com>
2023-05-15 18:38:25 -0400

    search page edits

--- client/src/Components/Card.js
+++ client/src/Components/Card.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Modal from "./Modal";
+const Card = ({ book }) => {
 
-function Card() {
-  return (
-    <div className="card">
-        <img src="../images/book.png" alt=""/>
-        <div className = "bottom">
-            <h3 className="title">Book</h3>
+    const [show,setShow]=useState(false);
+    const [bookItem,setItem]=useState();
+    console.log(book)
+    return (
+        <>
+            {
+                book.map((item) => {
+                    let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+                    let amount=item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+                    if(thumbnail!= undefined && amount !=undefined)
+                    {
+                        return (
+                            <>
+                            <div className="card" onClick={()=>{setShow(true);setItem(item)}}>
+                                <img src={thumbnail} alt="" />
+                                <div className="bottom">
+                                    <h3 className="title">{item.volumeInfo.title}</h3>
+                                </div>
+                            </div>
+                            </>
+                        )
+                    }
+                    
+                })
+            }
 
-        </div>
-    </div>
-  )
+        </>
+    )
 }
-
-export default Card
+export default Card;
--- client/src/routes/Search.js
+++ client/src/routes/Search.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import Card from '../components/Card'
+import axios from "axios"
 
 const Search = () => {
+  const [search, setSearch] = useState("")
+  const [bookData, setData]=useState([]);
+  const searchBook=(evt)=>{
+    if(evt.key==="Enter")
+    {
+        axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU'+'&maxResults=40')
+        .then(res=>setData(res.data.items))
+        .catch(err=>console.log(err))
+    }
+}
   return (
     <div>
       <div className="header">
           <h1>Search</h1>
       </div>
       <div className="search">
-        <input type="text" placeholder="Enter Your Book Name"></input>
+        <input type="text" placeholder="Enter Your Book Name" value={search} onChange={e => setSearch(e.target.value)} 
+        onKeyPress={searchBook} />
+        <button></button>
+
       </div>
       <div className="container">
-        <Card />  
+
+        <Card book={bookData}/>
+        <Card book={bookData}/>
+        <Card book={bookData}/>
+        <Card book={bookData}/>
+        <Card book={bookData}/>
+        <Card book={bookData}/>
+        <Card book={bookData}/>
+        <Card book={bookData}/>
       </div>
 
     </div>
