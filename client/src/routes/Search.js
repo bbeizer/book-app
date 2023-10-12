697bcbf26623dca5676bf256b183575cfcc5b569
bbeizer123 <ben.beizer@sycamoreinformatics.com>
2023-10-12 17:24:54 -0400

    improvements to UI

--- client/src/routes/Search.js
+++ client/src/routes/Search.js
@@ -5,12 +5,17 @@ import axios from "axios"
 const Search = () => {
   const [search, setSearch] = useState("")
   const [bookData, setData]=useState([]);
+
+  const fetchBooks = () => {
+    axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU'+'&maxResults=40')
+        .then(res=>setData(res.data.items))
+        .catch(err=>console.log(err))
+  };
+
   const searchBook=(evt)=>{
     if(evt.key==="Enter")
     {
-        axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU'+'&maxResults=40')
-        .then(res=>setData(res.data.items))
-        .catch(err=>console.log(err))
+      fetchBooks();
     }
 }
   return (
@@ -19,9 +24,9 @@ const Search = () => {
           <h1>Search</h1>
       </div>
       <div className="search">
-        <input type="text" placeholder="Enter Your Book Name" value={search} onChange={e => setSearch(e.target.value)} 
+        <input className="search-input" input type="text" placeholder="Enter Your Book Name" value={search} onChange={e => setSearch(e.target.value)} 
         onKeyPress={searchBook} />
-        <button></button>
+        <button className="search-button" onClick={fetchBooks}></button>
 
       </div>
       <div className="container">
