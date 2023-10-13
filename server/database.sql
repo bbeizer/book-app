9cc7bbf85cd778d688ba7cb4288fb57dc21a34b4
bbeizer123 <ben.beizer@sycamoreinformatics.com>
2023-10-12 21:05:41 -0400

    configured sequelize added to backend code

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,21 @@
-CREATE DATABASE bookapp;
-
-CREATE TABLE u(
+CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
+    email VARCHAR(255) UNIQUE NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Create books table
+CREATE TABLE books (
+    book_id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255),
+    description TEXT
+);
+
+CREATE TABLE user_books (
+    user_book_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    book_id INT REFERENCES books(book_id),
+    status VARCHAR(50)
 );
